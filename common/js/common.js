7d6181080ee15775e5d680faad5438768e693d09
hiroyuki nakajima <hiroyuki_nakajima@bita.jp>
2018-01-29 00:23:39 +0900

    RHN_TD-38 [中嶋][PC]JS課題１ https://bitadev.backlog.jp/view/RHN_TD-38 ▽課題2コミット

--- common/js/common.js
+++ common/js/common.js
@@ -1,40 +1,100 @@
 $(function () {
-	// var data = [
-	// 	{ id: '01', name: '田中', age: 20, team: '赤' },
-	// 	{ id: '02', name: '佐藤', age: 25, team: '白' },
-	// 	{ id: '03', name: '鈴木', age: 20, team: '赤' },
-	// 	{ id: '04', name: '山田', age: 25, team: '赤' },
-	// 	{ id: '05', name: '鈴木', age: 18, team: '白' }
-	// ]
 
-	// $.getJSON("data.json", function(data) {
-	// 	for (var i in data) {
-	// 		$('#output').append(
-	// 			'<tr>'
-	// 			+'<td>' + data[i].id + '</td>'
-	// 			+ '<td>' + data[i].name + '</td>'
-	// 			+ '<td>' + data[i].age + '</td>'
-	// 			+ '<td>' + data[i].team + '</td>'
-	// 			+'</tr>'
-	// 		);
-	// 	}
-	// });
-
-
+	//①
 	$.ajax({
 		type: 'GET',
 		url: 'http://localhost:3000/common/js/data.json',
 		success: function (data) {
+
 			for (var i in data) {
-				$('#output').append(
-					'<tr>'
-					+'<td>' + data[i].id + '</td>'
-					+ '<td>' + data[i].name + '</td>'
-					+ '<td>' + data[i].age + '</td>'
-					+ '<td>' + data[i].team + '</td>'
-					+'</tr>'
-				);
+				//$('#group').append(data[i].group);
+				//var grp = data[i].group;
+
+				$('.table > caption')
+					.append('<span class="isHidden tableText tableText--'+ data[i].group + '">' + data[i].group + '</span>');
+
+				for (var j in data[i].member) {
+					$('#output').append(
+						'<tr class="isHidden tr tr--'+ data[i].group +'">'
+						+ '<td>' + data[i].member[j].id + '</td>'
+						+ '<td>' + data[i].member[j].name + '</td>'
+						+ '<td>' + data[i].member[j].age + '</td>'
+						+ '<td>' + data[i].member[j].team + '</td>'
+						+ '</tr>'
+					);
+				}
 			}
+
+			function changeTable() {
+				//②
+		
+				//変数
+				var $tblText = $('.tableTitle > .tableText');
+				var $tr = $('#output > .tr')
+		
+				//表タイトル削除
+				function rmvTitle() {
+					$tblText.each(function () {
+						$(this).addClass('isHidden');
+					});
+				}
+				//表の列削除
+				function rmvTr() {
+					$tr.each(function () {
+						$(this).addClass('isHidden');
+					});
+				}
+		
+				//初期表示
+				//A表タイトル表示
+				$('.tableTitle > .tableText--A').each(function () {
+					$(this).removeClass('isHidden');
+				});
+				//A表データを表示
+				$('#output > .tr--A').each(function () {
+					$(this).removeClass('isHidden');
+				});
+		
+				//ボタンBを押す(A Cを非表示)
+				$('.button--B').on('click', function () {
+					//表タイトルをBに変更
+					// rmvTitle();
+					// $('.tableText--B').removeClass('isHidden');
+					// //Bのデータを表示
+					// rmvTr();
+					// $('.tr--B').removeClass('isHidden');
+
+					//表タイトル、表データを全非表示
+					rmvTitle();
+					rmvTr();
+					//Bのデータを表示
+					$('.tableText--B, .tr--B').removeClass('isHidden');
+
+				});
+		
+				//ボタンCを押す(A Bを非表示)
+				$('.button--C').on('click', function () {
+					//表タイトルをBに変更
+					rmvTitle();
+					$('.tableText--C').removeClass('isHidden');
+					//Cのデータを表示
+					rmvTr();
+					$('.tr--C').removeClass('isHidden');
+				});
+		
+				//ボタンAを押す(B Cを非表示)
+				$('.button--A').on('click', function () {
+					//表タイトルをAに変更
+					rmvTitle();
+					$('.tableText--A').removeClass('isHidden');
+					//Aの表データを表示
+					rmvTr();
+					$('.tr--A').removeClass('isHidden');
+				});
+			}
+			changeTable();
+
 		}
 	});
+
 }); //end
