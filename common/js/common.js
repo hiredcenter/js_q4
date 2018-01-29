eaa919bd21b9e3212127f7d883248d132602147e
hiroyuki nakajima <hiroyuki_nakajima@bita.jp>
2018-01-29 17:47:46 +0900

    RHN_TD-38 [中嶋][PC]JS課題１ https://bitadev.backlog.jp/view/RHN_TD-38 ▽課題4コミット

--- common/js/common.js
+++ common/js/common.js
@@ -1,30 +1,31 @@
 $(function () {
-
-	//①
+	//課題1
 	$.ajax({
 		type: 'GET',
 		url: 'http://localhost:3000/common/js/data.json',
 		success: function (data) {
 			for (var i in data) {
+				var compiled = _.template($('#lodashTemplateData').html());
 				//表タイトルの要素追加
 				$('.tbl > caption')
-					.append('<span class="isHidden tblText tblText--'+ data[i].group + '">' + data[i].group + '</span>');
-				//表の各列追加
+					.append('<span class="isHidden tblText tblText-'+ data[i].group + '">' + data[i].group + '</span>');
+					//表の各列追加
 				for (var j in data[i].member) {
 					$('#output').append(
-						'<tr class="isHidden tr tr--'+ data[i].group +'">'
-						+ '<td>' + data[i].member[j].id + '</td>'
-						+ '<td>' + data[i].member[j].name + '</td>'
-						+ '<td>' + data[i].member[j].age + '</td>'
-						+ '<td>' + data[i].member[j].team + '</td>'
-						+ '</tr>'
+						//課題4 underscore.jsのテンプレート用データ
+						compiled({
+							"group": 'class="isHidden tr tr-' + data[i].group +'"',
+							"id": data[i].member[j].id,
+							"name": data[i].member[j].name,
+							"age": data[i].member[j].age,
+							"team": data[i].member[j].team,
+						})
 					);
 				}
 			}
 
-			//②
+			//課題2
 			function changeTbl() {
-				//変数
 				var $tblText = $('.tblTitle > .tblText');
 				var $tr = $('#output > .tr')
 
@@ -39,43 +40,33 @@ $(function () {
 				}
 
 				//初期表示設定(A表示)
-				$('.tblText--A').removeClass('isHidden');
-				$('#output > .tr--A').each(function () {
+				$('.tblText-A').removeClass('isHidden');
+				$('#output > .tr-A').each(function () {
 					$(this).removeClass('isHidden');
 				});
 
 				//ボタンBを押す(A Cを非表示)
-				$('.btn--B').on('click', function () {
+				$('.btn-B').on('click', function () {
 					//表タイトル・表データを全非表示→Bのデータを表示
 					rmvTitleTr();
-					$('.tblText--B, .tr--B').removeClass('isHidden');
+					$('.tblText-B, .tr-B').removeClass('isHidden');
 				});
 
 				//ボタンCを押す(A Bを非表示)
-				$('.btn--C').on('click', function () {
+				$('.btn-C').on('click', function () {
 					//表タイトル・表データを全非表示→Cのデータを表示
 					rmvTitleTr();
-					$('.tblText--C, .tr--C').removeClass('isHidden');
+					$('.tblText-C, .tr-C').removeClass('isHidden');
 				});
 
 				//ボタンAを押す(B Cを非表示)
-				$('.btn--A').on('click', function () {
+				$('.btn-A').on('click', function () {
 					//表タイトル・表データを全非表示→Aのデータを表示
 					rmvTitleTr();
-					$('.tblText--A, .tr--A').removeClass('isHidden');
+					$('.tblText-A, .tr-A').removeClass('isHidden');
 				});
 			}
 			changeTbl();
-
-
-			//③
-			// var $acd = $('.accordionTitle > a');
-
-			// $acd.on('click', function () {
-				
-
-			// }
 		}
 	});
-
-}); //end
+});
