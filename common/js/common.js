9d5067eba849874aef46ad1aa1852bb791967437
hiroyuki nakajima <hiroyuki_nakajima@bita.jp>
2018-01-31 12:49:49 +0900

    RHN_TD-38 [中嶋][PC]JS課題１ https://bitadev.backlog.jp/view/RHN_TD-38#comment-1188219199 ▽FB修正(【JS】4以外完了)

--- common/js/common.js
+++ common/js/common.js
@@ -2,71 +2,88 @@ $(function () {
 	//課題1
 	$.ajax({
 		type: 'GET',
-		url: 'http://localhost:3000/common/js/data.json',
-		success: function (data) {
-			for (var i in data) {
-				var compiled = _.template($('#lodashTemplateData').html());
-				//表タイトルの要素追加
-				$('.tbl > caption')
-					.append('<span class="isHidden tblText tblText-'+ data[i].group + '">' + data[i].group + '</span>');
-					//表の各列追加
-				for (var j in data[i].member) {
-					$('#output').append(
-						//課題4 underscore.jsのテンプレート用データ
-						compiled({
-							"group": 'class="isHidden tr tr-' + data[i].group +'"',
-							"id": data[i].member[j].id,
-							"name": data[i].member[j].name,
-							"age": data[i].member[j].age,
-							"team": data[i].member[j].team,
-						})
-					);
-				}
-			}
+		url: './common/js/data.json',
+		'dataType': 'json',
+	}).done(function (data) {
+		$.each(data, function(i, group) {
+			var compiled = _.template($('#lodashTemplateData').html());
+			//表タイトルの要素追加
+			$('.tbl > caption')
+				.append('<span class="isHidden tblText tblText-' + data[i].group + '">' + data[i].group + '</span>');
 
-			//課題2
-			function changeTbl() {
-				var $tblText = $('.tblTitle > .tblText');
-				var $tr = $('#output > .tr')
+			$.each(data, function(j, member) {
+				$('#output').append(
+					//課題4 underscore.jsのテンプレート用データ
+					compiled({
+						"group": 'class="isHidden tr tr-' + data[i].group + '"',
+						"id": data[i].member[j].id,
+						"name": data[i].member[j].name,
+						"age": data[i].member[j].age,
+						"team": data[i].member[j].team,
+					})
+				);
+			});
+		});
 
-				//表タイトル・列削除
-				function rmvTitleTr() {
-					$tblText.each(function () {
-						$(this).addClass('isHidden');
-					});
-					$tr.each(function () {
-						$(this).addClass('isHidden');
-					});
-				}
+		//課題2
+		function changeTbl() {
+			var $tblText = $('.tblTitle > .tblText');
+			var $tr = $('#output > .tr')
 
-				//初期表示設定(A表示)
-				$('.tblText-A').removeClass('isHidden');
-				$('#output > .tr-A').each(function () {
-					$(this).removeClass('isHidden');
+			//表タイトル・列削除
+			function rmvTitleTr() {
+				$tblText.each(function () {
+					$(this).addClass('isHidden');
 				});
-
-				//ボタンBを押す(A Cを非表示)
-				$('.btn-B').on('click', function () {
-					//表タイトル・表データを全非表示→Bのデータを表示
-					rmvTitleTr();
-					$('.tblText-B, .tr-B').removeClass('isHidden');
-				});
-
-				//ボタンCを押す(A Bを非表示)
-				$('.btn-C').on('click', function () {
-					//表タイトル・表データを全非表示→Cのデータを表示
-					rmvTitleTr();
-					$('.tblText-C, .tr-C').removeClass('isHidden');
-				});
-
-				//ボタンAを押す(B Cを非表示)
-				$('.btn-A').on('click', function () {
-					//表タイトル・表データを全非表示→Aのデータを表示
-					rmvTitleTr();
-					$('.tblText-A, .tr-A').removeClass('isHidden');
+				$tr.each(function () {
+					$(this).addClass('isHidden');
 				});
 			}
-			changeTbl();
+
+			//初期表示設定(A表示)
+			$('.tblText-A').removeClass('isHidden');
+			$('#output > .tr-A').each(function () {
+				$(this).removeClass('isHidden');
+			});
+
+			//ボタンBを押す(A Cを非表示)
+			$('.btn-B').on('click', function () {
+				//表タイトル・表データを全非表示→Bのデータを表示
+				rmvTitleTr();
+				$('.tblText-B, .tr-B').removeClass('isHidden');
+			});
+
+			//ボタンCを押す(A Bを非表示)
+			$('.btn-C').on('click', function () {
+				//表タイトル・表データを全非表示→Cのデータを表示
+				rmvTitleTr();
+				$('.tblText-C, .tr-C').removeClass('isHidden');
+			});
+
+			//ボタンAを押す(B Cを非表示)
+			$('.btn-A').on('click', function () {
+				//表タイトル・表データを全非表示→Aのデータを表示
+				rmvTitleTr();
+				$('.tblText-A, .tr-A').removeClass('isHidden');
+			});
+
+
+			//汎用化
+			//ボタンAを押す(B Cを非表示)
+			$('.btn-A').on('click', function () {
+				//表タイトル・表データを全非表示→Aのデータを表示
+				rmvTitleTr();
+				$('.tblText-A, .tr-A').removeClass('isHidden');
+			});
 		}
+		changeTbl();
+
+
+
+		//課題3
+		$('.acdIcon').on('click', function () {
+			$(this).toggleClass('is-active');
+			$(this).next().toggleClass('is-hidden');
+		});
 	});
 });
